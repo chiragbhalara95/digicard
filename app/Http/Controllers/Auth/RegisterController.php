c913dc6e49ea18f62b68e0bbca920a0060196f27
chiragbhalara95 <chiragbhalara95@gmail.com>
2021-03-21 17:45:27 +0530

    Store User phone no for register.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,9 +65,11 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'name'         => $data['name'],
+            'email'        => $data['email'],
+            'country_code' => $data['country_code'],
+            'phone'        => $data['phone'],
+            'password'     => Hash::make($data['password']),
         ]);
     }
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'is_admin'
+        'is_admin',
+        'phone',
+        'country_code'
     ];
 
     /**
