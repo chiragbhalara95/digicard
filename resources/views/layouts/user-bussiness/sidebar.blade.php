c720e900a1b021aad4cd31f769fa2601111842f8
Chirag Bhalara <chiragbhalara.nyusoft@gmail.com>
2022-08-06 17:45:43 +0530

    Implemented User Setting

--- resources/views/layouts/user-bussiness/sidebar.blade.php
+++ resources/views/layouts/user-bussiness/sidebar.blade.php
@@ -91,6 +91,15 @@
             </a>
           </li>
 
+          <li class="nav-item">
+            <a href="{{route('getUserConfigure')}}" class="nav-link">
+              <i class="nav-icon fas fa-user"></i>
+              <p>
+                User Setting
+              </p>
+            </a>
+          </li>
+
           @if(Auth::check())
           <li class="nav-item">
             <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('frm-logout').submit();">
