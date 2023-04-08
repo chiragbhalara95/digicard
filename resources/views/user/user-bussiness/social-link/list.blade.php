a78b65dafbcab858f1b1490a66cc9bcbbdd3be15
Chirag Bhalara <chiragbhalara.nyusoft@gmail.com>
2023-04-08 23:59:55 +0530

    change link

--- resources/views/user/user-bussiness/social-link/list.blade.php
+++ resources/views/user/user-bussiness/social-link/list.blade.php
@@ -13,7 +13,7 @@
         </div>
         <ul class="app-breadcrumb breadcrumb">
             <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
-            <li class="breadcrumb-item"><a href="{{url('product')}}">Gallery List</a></li>
+            <li class="breadcrumb-item"><a href="{{route('business.social-media-master-list')}}">Social Media Link List</a></li>
         </ul>
     </div>
 
