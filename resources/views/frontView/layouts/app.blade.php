adfb1169f58e7a0fe9e4deb9a9a4756083835e26
Chirag Bhalara <chiragbhalara95@gmail.com>
2025-05-03 09:41:12 -0700

    added chatra

--- resources/views/frontView/layouts/app.blade.php
+++ resources/views/frontView/layouts/app.blade.php
@@ -239,6 +239,21 @@
   gtag('config', 'G-CJZJHWL0WG');
 </script>
 
+<!-- Chatra {literal} -->
+<script>
+    (function(d, w, c) {
+        w.ChatraID = 'F9hhbRFnXZ3uwsvAu';
+        var s = d.createElement('script');
+        w[c] = w[c] || function() {
+            (w[c].q = w[c].q || []).push(arguments);
+        };
+        s.async = true;
+        s.src = 'https://call.chatra.io/chatra.js';
+        if (d.head) d.head.appendChild(s);
+    })(document, window, 'Chatra');
+</script>
+<!-- /Chatra {/literal} -->
+
 
    </body>
 </html>
